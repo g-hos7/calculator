958d85a4ccd73da8dcc36d26e9d68577a7d55ae3
Tim <tim.c.barnes@gmail.com>
2020-08-29 23:43:16 -0500

    Add core functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,78 @@
+const display = document.querySelector('#display');
+const number = document.createElement('div');
+display.appendChild(number).id = 'text';
+
+function numberSelect(display) {
+  document.querySelectorAll('.num').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      display.innerText += btn.innerText;
+    });
+  });
+}
+
+function backspace(display) {
+  document.querySelector('#backspace').addEventListener('click', () => {
+    display.innerText = display.innerText.replace(/.$/, '');
+  });
+}
+
+function clearSelect(display) {
+  document.querySelector('#clear').addEventListener('click', () => {
+    display.innerText = '';
+    operator = '';
+    num1 = NaN;
+  });
+}
+
+function signSelect(display) {
+  document.querySelector('#sign').addEventListener('click', () => {
+    if(display.innerText.includes('-')) {
+      display.innerText = display.innerText.replace('-', '');
+    } else {
+      display.innerText = '-' + display.innerText;
+    }
+  });
+}
+
+function operatorSelect(display) {
+  document.querySelectorAll('.sideBtns').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      operator = btn.id;
+      num1 = parseFloat(display.innerText);
+      display.innerText = '';
+    });
+  });
+}
+
+function equalSelect(display) {
+  document.querySelector('#equals').addEventListener('click', () => {
+    display.innerText = operate(operator, num1, parseFloat(display.innerText));
+  });
+}
+
 function operate(operator, num1, num2) {
+  let result = 0;
   switch(operator) {
     case "add":
-      add(num1, num2);
+      result = add(num1, num2);
       break;
     case "subtract":
-      subtract(num1, num2);
+      result = subtract(num1, num2);
       break;
     case "multiply":
-      multiply(num1, num2);
+      result = multiply(num1, num2);
       break;
     case "divide":
-      divide(num1, num2);
+      result = divide(num1, num2);
       break;
     default:
-      console.log("Invalid operator");
+      console.log(operator);
+      result = "ERROR"
+  }
+  if(typeof result === "string") {
+    return result;
+  } else {
+    return result.toFixed(2);
   }
 }
 
@@ -30,5 +89,22 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  if(num2 === 0) {
+    return "DIVISION ERROR";
+  } else {
+    return num1 / num2;
+  }
 }
+
+function calculate() {
+  let num1 = NaN;
+  let operator = '';
+  signSelect(number);
+  numberSelect(number);
+  backspace(number);
+  clearSelect(number);
+  operatorSelect(number);
+  equalSelect(number);
+}
+
+calculate();
